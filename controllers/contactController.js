5cecee4e60b167039fd8721605d7241fb737fe4c
Narendra Babu <narendrab077@gmail.com>
2026-05-10 20:20:46 +0530

    improve contact controller

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,53 +1,73 @@
 const Contact = require("../models/Contact");
 
-
-// Add new contact
+/* ---------------- ADD CONTACT ---------------- */
 exports.addContact = async (req, res) => {
   try {
     const { userId, contactId } = req.body;
+
+    // Prevent adding yourself
+    if (userId === contactId) {
+      return res.status(400).json({
+        message: "You cannot add yourself",
+      });
+    }
 
     // Prevent duplicate contacts
     const existingContact =
       await Contact.findOne({
         userId,
-        contactId
+        contactId,
       });
 
     if (existingContact) {
-      return res.json({
-        message: "Contact already added"
+      return res.status(400).json({
+        message:
+          "Contact already added",
       });
     }
 
     const newContact =
       await Contact.create({
         userId,
-        contactId
+        contactId,
       });
 
-    res.status(201).json(newContact);
-
+    res.status(201).json({
+      message:
+        "Contact added successfully",
+      contact: newContact,
+    });
   } catch (error) {
+    console.log(error);
+
     res.status(500).json({
-      message: error.message
+      message:
+        error.message,
     });
   }
 };
 
-
-// Get all saved contacts
+/* ---------------- GET CONTACTS ---------------- */
 exports.getContacts = async (req, res) => {
   try {
     const contacts =
       await Contact.find({
-        userId: req.params.userId
-      }).populate("contactId");
+        userId:
+          req.params.userId,
+      }).populate(
+        "contactId",
+        "name email"
+      );
 
-    res.status(200).json(contacts);
-
+    res.status(200).json(
+      contacts
+    );
   } catch (error) {
+    console.log(error);
+
     res.status(500).json({
-      message: error.message
+      message:
+        error.message,
     });
   }
 };
--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,12 +1,27 @@
-const express = require("express");
-const router = express.Router();
+const express =
+  require("express");
+
+const router =
+  express.Router();
 
 const {
   addContact,
+  getContacts,
+} = require(
+  "../controllers/contactController"
+);
+
+/* Add new contact */
+router.post(
+  "/add",
+  addContact
+);
+
+/* Get all contacts for user */
+router.get(
+  "/:userId",
   getContacts
-} = require("../controllers/contactController");
+);
 
-router.post("/add", addContact);
-router.get("/:userId", getContacts);
-
-module.exports = router;
+module.exports =
+  router;
